4e56ea0a4a8b862367d357de7a322a8d34e27b1f
Sascha Greve <sagr4019@users.noreply.github.com>
2017-02-24 22:48:29 +0100

    Interface functions

--- Jaws_Service_Library/IService1.cs
+++ Jaws_Service_Library/IService1.cs
@@ -15,36 +15,190 @@ namespace Jaws_Service_Library
         [OperationContract]
         string GetData(int value);
 
-        [OperationContract]
-        Artikel getArtikel(int value);
-       
+        //Personal
 
         [OperationContract]
-        CompositeType GetDataUsingDataContract(CompositeType composite);
+        Personal getPersonalbyId(int id);
+
+        [OperationContract]
+        void setPersonal(Personal person);
+
+        [OperationContract]
+        void updatePersonal(Personal personal);
+
+        [OperationContract]
+        List<Personal> getPersonalList();
+
+        //Arbeitsvertrag
+
+        [OperationContract]
+        Arbeitsvertrag getArbeitsvertragbyId(int id);
+
+        [OperationContract]
+        void setArbeitsvertrag(Arbeitsvertrag vertrag);
+
+        [OperationContract]
+        void updateArbeitsvertrag(Arbeitsvertrag arbeitsvertrag);
+
+        [OperationContract]
+        List<Arbeitsvertrag> getArbeitsvertragList();
+
+        //Rolle
+
+        [OperationContract]
+        List<Rolle> getRollebyId(int id);
+
+        [OperationContract]
+        void setRolle(Rolle role);
+
+        [OperationContract]
+        void updateRolle(Rolle rolle);
+
+        [OperationContract]
+        List<Rolle> getRolleList();
+
+        [OperationContract]
+        List<Rolle> getRollebyPersonalId(int id);
+
+        //Recht
+
+        [OperationContract]
+        List<Recht> getRechtbyId(int id);
+
+        [OperationContract]
+        void setRecht(Recht recht);
+
+        [OperationContract]
+        void updateRecht(Recht recht);
+
+        [OperationContract]
+        List<Recht> getRechtbyRolleId(int id);
+
+        //Schicht
+
+        [OperationContract]
+        List<Schicht> getSchichtbyId(int id);
+
+        [OperationContract]
+        void setSchicht(Schicht schicht);
+
+        [OperationContract]
+        void updateSchicht(Schicht schicht);
+
+        [OperationContract]
+        List<Schicht> getSchichtbyPersonalId(int id);
+
+        //Lieferant
+
+        [OperationContract]
+        Lieferant getLieferantbyId(int id);
+
+        [OperationContract]
+        void setLieferant(Lieferant lieferant);
+
+        [OperationContract]
+        void updateLieferant(Lieferant lieferant);
+
+        [OperationContract]
+        List<Lieferant> getLieferantList();
+
+        [OperationContract]
+        Lieferant getLieferantbyArtikelId(int id);
+
+        //Artikel
+
+        [OperationContract]
+        Artikel getArtikelbyId(int id);
+
+        [OperationContract]
+        void setArtikel(Artikel artikel);
+
+        [OperationContract]
+        void updateArtikel(Artikel artikel);
+
+        [OperationContract]
+        List<Artikel> getArtikelList();
+
+        [OperationContract]
+        List<Artikel> getArtikelbyLieferantId(int id);
+
+        [OperationContract]
+        List<Artikel> getArtikelbyWarengruppeId(int id);
+
+        [OperationContract]
+        List<Artikel> getArtikelbyBelegId(int id);
+
+        //Warengruppe
+
+        [OperationContract]
+        Warengruppe getWarengruppebyId(int id);
+
+        [OperationContract]
+        void setWarengruppe(Warengruppe gruppe);
+
+        [OperationContract]
+        void updateWarengruppe(Warengruppe warengruppe);
+
+        [OperationContract]
+        List<Warengruppe> getWarengruppeList();
+
+        [OperationContract]
+        Warengruppe getWarengruppebyName(String name);
+
+        //Prognose
+
+        [OperationContract]
+        Prognose getPrognosebyId(int id);
+
+        [OperationContract]
+        void setPrognose(Prognose prognose);
+
+        [OperationContract]
+        void updatePrognose(Prognose prognose);
+
+        [OperationContract]
+        List<Prognose> getPrognoseList();
+
+        [OperationContract]
+        Prognose getPrognosebyArtikelId(int id);
+
+        [OperationContract]
+        List<Prognose> getPrognosebyDate(DateTime date);
+
+        //Beleg
+
+        [OperationContract]
+        Beleg getBelegbyId(int id);
+
+        [OperationContract]
+        void setBeleg(Beleg beleg);
+
+        [OperationContract]
+        void updateBeleg(Beleg beleg);
+
+        [OperationContract]
+        List<Beleg> getBelegList();
+
+        [OperationContract]
+        List<Beleg> getBelegbyArtikelId(int id);
+
+        //Lieferart
+
+        [OperationContract]
+        Lieferart getLieferartbyId(int id);
+
+        [OperationContract]
+        void setLieferart(Lieferart lieferart);
+
+        [OperationContract]
+        void updateLieferart(Lieferart lieferart);
+
+        [OperationContract]
+        List<Lieferart> getLieferartList();
+
+
 
         // TODO: Hier Dienstvorgänge hinzufügen
     }
 
-    // Verwenden Sie einen Datenvertrag, wie im folgenden Beispiel dargestellt, um Dienstvorgängen zusammengesetzte Typen hinzuzufügen.
-    // Sie können im Projekt XSD-Dateien hinzufügen. Sie können nach dem Erstellen des Projekts dort definierte Datentypen über den Namespace "Jaws_Service_Library.ContractType" direkt verwenden.
-    [DataContract]
-    public class CompositeType
-    {
-        bool boolValue = true;
-        string stringValue = "Hello ";
-
-        [DataMember]
-        public bool BoolValue
-        {
-            get { return boolValue; }
-            set { boolValue = value; }
-        }
-
-        [DataMember]
-        public string StringValue
-        {
-            get { return stringValue; }
-            set { stringValue = value; }
-        }
-    }
 }
--- Jaws_Service_Library/Service1.cs
+++ Jaws_Service_Library/Service1.cs
@@ -17,6 +17,17 @@ namespace Jaws_Service_Library
             db = new DataContainer();
             db.Configuration.ProxyCreationEnabled = false;
         }
+
+        public Arbeitsvertrag getArbeitsvertragbyId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Arbeitsvertrag> getArbeitsvertragList()
+        {
+            throw new NotImplementedException();
+        }
+
         public Artikel getArtikel(int value)
         {
             
@@ -25,22 +36,264 @@ namespace Jaws_Service_Library
             throw new NotImplementedException();
         }
 
-        public string GetData(int value)
+        public List<Artikel> getArtikelbyBelegId(int id)
         {
-            return string.Format("You entered: {0}", value);
+            throw new NotImplementedException();
         }
 
-        public CompositeType GetDataUsingDataContract(CompositeType composite)
+        public Artikel getArtikelbyId(int id)
         {
-            if (composite == null)
-            {
-                throw new ArgumentNullException("composite");
-            }
-            if (composite.BoolValue)
-            {
-                composite.StringValue += "Suffix";
-            }
-            return composite;
+            throw new NotImplementedException();
+        }
+
+        public List<Artikel> getArtikelbyLieferantId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Artikel> getArtikelbyWarengruppeId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Artikel> getArtikelList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Beleg> getBelegbyArtikelId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Beleg getBelegbyId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Beleg> getBelegList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public string GetData(int value)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Lieferant getLieferantbyArtikelId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Lieferant getLieferantbyId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Lieferant> getLieferantList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Lieferart getLieferartbyId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Lieferart> getLieferartList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Personal getPersonalbyId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Personal> getPersonalList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Prognose getPrognosebyArtikelId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Prognose> getPrognosebyDate(DateTime date)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Prognose getPrognosebyId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Prognose> getPrognoseList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Recht> getRechtbyId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Recht> getRechtbyRolleId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Rolle> getRollebyId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Rolle> getRollebyPersonalId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Rolle> getRolleList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Schicht> getSchichtbyId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Schicht> getSchichtbyPersonalId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Warengruppe getWarengruppebyId(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Warengruppe getWarengruppebyName(string name)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Warengruppe> getWarengruppeList()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setArbeitsvertrag(Arbeitsvertrag vertrag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setArtikel(Artikel artikel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setBeleg(Beleg beleg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setLieferant(Lieferant lieferant)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setLieferart(Lieferart lieferart)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setPersonal(Personal person)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setPrognose(Prognose prognose)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setRecht(Recht recht)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setRolle(Rolle role)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setSchicht(Schicht schicht)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void setWarengruppe(Warengruppe gruppe)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void updateArbeitsvertrag(Arbeitsvertrag arbeitsvertrag)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void updateArtikel(Artikel artikel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void updateBeleg(Beleg beleg)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void updateLieferant(Lieferant lieferant)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void updateLieferart(Lieferart lieferart)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void updatePersonal(Personal personal)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void updatePrognose(Prognose prognose)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void updateRecht(Recht recht)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void updateRolle(Rolle rolle)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void updateSchicht(Schicht schicht)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void updateWarengruppe(Warengruppe warengruppe)
+        {
+            throw new NotImplementedException();
         }
     }
 }
