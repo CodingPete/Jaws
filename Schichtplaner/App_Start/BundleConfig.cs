c0da07409f76993e90554139a7800ffb6e8e8933
jubr9578 <julian.brueggemann@gmx.net>
2017-03-01 11:32:07 +0100

    layout

--- Schichtplaner/App_Start/BundleConfig.cs
+++ Schichtplaner/App_Start/BundleConfig.cs
@@ -26,6 +26,43 @@ namespace Schichtplaner
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new StyleBundle("~/Template/Style").Include(
+                        "~/Content/template2/styles/datatables/dataTables.bootstrap4.css",
+                        "~/Content/template2/styles/sweetalert/sweetalert.css",
+                        "~/Content/template2/styles/fullcalendar/fullcalendar.min.css",
+                        "~/Content/template2/styles/bootstrap/bootstrap.css",
+                        "~/Content/template2/styles/font-awesome/font-awesome.css",
+                        "~/Content/template2/styles/animate.css/animate.css",
+                        "~/Content/template2/styles/app.css",
+                        "~/Content/template2/styles/app.skins.css"
+                ));
+
+            bundles.Add(new ScriptBundle("~/Template/Scripts").Include(
+                "~/Content/template2/scripts/jquery/jquery.js",
+                "~/Content/template2/scripts/tether/tether.js",
+                "~/Content/template2/scripts/bootstrap/bootstrap.js",
+                "~/Content/template2/scripts/fastclick/fastclick.js",
+                "~/Content/template2/scripts/constants.js",
+                "~/Content/template2/scripts/main.js",
+
+                "~/Content/template2/scripts/dashboard.js",
+                "~/Content/template2/scripts/datatables/jquery.dataTables.js",
+                "~/Content/template2/scripts/datatables/dataTables.bootstrap4.js",
+                "~/Content/template2/scripts/sweetalert/sweetalert.min.js",
+
+                "~/Content/template2/scripts/moment/moment.min.js",
+                "~/Content/template2/scripts/jquery.ui/core.js",
+                "~/Content/template2/scripts/jquery.ui/widget.js",
+                "~/Content/template2/scripts/jquery.ui/mouse.js",
+                "~/Content/template2/scripts/jquery.ui/draggable.js",
+                "~/Content/template2/scripts/jqueryui-touch-punch/jquery.ui.touch-punch.js",
+                "~/Content/template2/scripts/fullcalendar/fullcalendar.min.js",
+                "~/Content/template2/scripts/fullcalendar/gcal.js",
+
+                "~/Content/template2/scripts/calendar.js"
+
+            ));
         }
     }
 }
