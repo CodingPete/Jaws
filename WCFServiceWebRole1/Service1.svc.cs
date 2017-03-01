d0f86dc634672dfbf6320cc6edb25e10cc1771da
Sascha Greve <sascha-privat@posteo.de>
2017-03-01 17:20:51 +0100

    Service Fix

--- WCFServiceWebRole1/Service1.svc.cs
+++ WCFServiceWebRole1/Service1.svc.cs
@@ -270,24 +270,20 @@ namespace WCFServiceWebRole1
 
         public void setArtikelBeleg(Artikel artikel, Beleg beleg)
         {
+            Lieferart lfa = this.getLieferartbyId(beleg.LieferartId);
+            if (lfa.Name.Equals("Verkauf"))
+            {
+                artikel.Bestand -= 1;
+                this.updateArtikel(artikel);
+            }
             db.ArtikelBelegSet.Add(new ArtikelBeleg() { ArtikelId = artikel.Id, BelegId = beleg.Id });
             db.SaveChanges();
-
         }
 
         public int setBeleg(Beleg beleg)
         {
             Lieferart lfa = this.getLieferartbyId(beleg.LieferartId);
             beleg.Lieferart = lfa;
-            var artikel = this.getArtikelbyBelegId(beleg.Id);
-            if (lfa.Name.Equals("Verkauf"))
-            {
-                artikel.ForEach(item =>
-                {
-                    item.Bestand = item.Bestand - 1;
-                    this.updateArtikel(item);
-                });
-            }
             beleg.Lieferart.Beleg.Add(beleg);
             db.BelegSet.Add(beleg);
             db.SaveChanges();
@@ -378,6 +374,10 @@ namespace WCFServiceWebRole1
 
         public void updateArtikel(Artikel artikel)
         {
+            //var curartikel = db.ArtikelSet.Find(artikel.Id);
+            //curartikel = artikel;
+            
+
             db.ArtikelSet.Attach(artikel);
             var entry = db.Entry(artikel);
             entry.State = System.Data.Entity.EntityState.Modified;
