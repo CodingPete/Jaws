8a62ba2abccf2a562a401b751c975c72357034c7
jubr9578 <julian.brueggemann@gmx.net>
2017-03-02 19:15:36 +0100

    CLEAN UP CODE

--- Schichtplaner/App_Start/BundleConfig.cs
+++ Schichtplaner/App_Start/BundleConfig.cs
@@ -8,46 +8,25 @@ namespace Schichtplaner
         // Weitere Informationen zu Bundling finden Sie unter "http://go.microsoft.com/fwlink/?LinkId=301862"
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
-
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Verwenden Sie die Entwicklungsversion von Modernizr zum Entwickeln und Erweitern Ihrer Kenntnisse. Wenn Sie dann
-            // für die Produktion bereit sind, verwenden Sie das Buildtool unter "http://modernizr.com", um nur die benötigten Tests auszuwählen.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js",
-                      "~/Scripts/respond.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
-
             bundles.Add(new StyleBundle("~/Template/Style").Include(
-            "~/Content/template2/styles/datatables/dataTables.bootstrap4.min.css",
-            "~/Content/template2/styles/datatables/responsive.bootstrap4.min.css",
-            "~/Content/template2/styles/sweetalert/sweetalert.css",
-            "~/Content/template2/styles/fullcalendar/fullcalendar.min.css",
-            "~/Content/template2/styles/bootstrap/bootstrap.css",
-            "~/Content/template2/styles/font-awesome/font-awesome.css",
-            "~/Content/template2/styles/animate.css/animate.css",
-            "~/Content/template2/styles/app.css",
-            "~/Content/template2/styles/app.skins.css"
-    ));
+                "~/Content/template2/styles/datatables/dataTables.bootstrap4.min.css",
+                "~/Content/template2/styles/datatables/responsive.bootstrap4.min.css",
+                "~/Content/template2/styles/sweetalert/sweetalert.css",
+                "~/Content/template2/styles/fullcalendar/fullcalendar.min.css",
+                "~/Content/template2/styles/bootstrap/bootstrap.css",
+                "~/Content/template2/styles/font-awesome/font-awesome.css",
+                "~/Content/template2/styles/animate.css/animate.css",
+                "~/Content/template2/styles/app.css",
+                "~/Content/template2/styles/app.skins.css"
+            ));
 
             bundles.Add(new ScriptBundle("~/Template/Scripts").Include(
+                "~/Content/template2/scripts/modernizr/modernizr-2.6.2.js",
                 "~/Content/template2/scripts/jquery/jquery.js",
                 "~/Content/template2/scripts/tether/tether.js",
                 "~/Content/template2/scripts/bootstrap/bootstrap.js",
+                "~/Content/template2/scripts/bootstrap/respond.js",
                 "~/Content/template2/scripts/fastclick/fastclick.js",
-                "~/Content/template2/scripts/constants.js",
-                "~/Content/template2/scripts/main.js",
-
-                "~/Content/template2/scripts/dashboard.js",
 
                 "~/Content/template2/scripts/datatables/jquery.dataTables.min.js",
                 "~/Content/template2/scripts/datatables/dataTables.bootstrap4.min.js",
@@ -64,10 +43,15 @@ namespace Schichtplaner
                 "~/Content/template2/scripts/jqueryui-touch-punch/jquery.ui.touch-punch.js",
                 "~/Content/template2/scripts/fullcalendar/fullcalendar.min.js",
                 "~/Content/template2/scripts/fullcalendar/lang-all.js",
-                "~/Content/template2/scripts/fullcalendar/gcal.js",
+                "~/Content/template2/scripts/fullcalendar/gcal.js"
+            ));
 
-                "~/Content/template2/scripts/calendar.js"
+            bundles.Add(new ScriptBundle("~/Template/Main").Include(
+                "~/Content/template2/scripts/main.js"
+            ));
 
+            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
+                "~/Content/template2/scripts/jquery/jquery.validate*"
             ));
         }
     }
