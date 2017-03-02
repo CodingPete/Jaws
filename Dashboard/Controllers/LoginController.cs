b5932110dcfee794eac35c82337e689150f04996
Sascha Greve <sascha-privat@posteo.de>
2017-03-02 22:20:33 +0100

    Release Login Only user with pw and mail can login

--- Dashboard/Controllers/LoginController.cs
+++ Dashboard/Controllers/LoginController.cs
@@ -20,16 +20,16 @@ namespace Dashboard.Controllers
         }
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Index([Bind(Include = "Username")] String temp) //TODO: Bind Ersetzen, keine Ahnung was man da machen kann
+        public ActionResult Index([Bind(Include = "Username")] String temp) 
         {
             var post = Request.Form;
             String username = post["Username"];
             String password = post["Password"];
             
-            if (new MyMembershipProvider().ValidateUser(username,password))
+            if (new MyMembershipProvider().ValidateUser(username,password)) //Prüfe ob eingegebene Benutzereingaben gültig sind
             {
                 Personal person = db.PersonalSet.First((x) => x.email == username);
-                FormsAuthentication.RedirectFromLoginPage(person.email, false);
+                FormsAuthentication.RedirectFromLoginPage(person.email, false); 
                 //create a Session cookie
                 Session["Name"] = person.Name;
                 Session["Vorname"] = person.Vorname;
