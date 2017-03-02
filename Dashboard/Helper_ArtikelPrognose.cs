e420980d6facd6359fb464ab02f67e2f1c2b1e58
Sascha Greve <sascha-privat@posteo.de>
2017-03-02 18:56:13 +0100

    Modal für bestellungen

---
+++ Dashboard/Helper_ArtikelPrognose.cs
@@ -0,0 +1,14 @@
+﻿using DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Dashboard
+{
+    public class Helper_ArtikelPrognose
+    {
+        public int prognosemenge { get; set; } 
+        public Artikel artikel { get; set; }
+    }
+}
